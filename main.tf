df8f5bc4b7e9dcbdf1d4355aa2b59ed6e0f95240
Reragardious <matthee.alex233@gmail.com>
2024-05-10 17:02:50 +1200

    Update main.tf
    Creating an additional three subnets for the backend, frontend, and middle needed to use the load balancer.

--- main.tf
+++ main.tf
@@ -3,8 +3,32 @@
 
 #Create a Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "GlobalTrust Bank"
+  name                = "globaltrust_banktesting"
   address_space       = ["10.0.0.0/16"]
   location            = "East US"
   resource_group_name = "1-0ebe8a23-playground-sandbox"
+}
+
+#Create a subnet for frontened
+resource "azurerm_subnet" "subnet" {
+  name                 = "globaltrust_bank_frontend_subnet"
+  resource_group_name  = "1-0ebe8a23-playground-sandbox"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes       = ["10.0.1.0/24"]
+}
+
+#Create a subnet for middle
+resource "azurerm_subnet" "subnet" {
+  name                 = "globaltrust_bank_middle_subnet"
+  resource_group_name  = "1-0ebe8a23-playground-sandbox"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes       = ["10.0.1.0/24"]
+}
+
+#Create a subnet for backend
+resource "azurerm_subnet" "subnet" {
+  name                 = "globaltrust_bank_backend_subnet"
+  resource_group_name  = "1-0ebe8a23-playground-sandbox"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes       = ["10.0.1.0/24"]
 }
