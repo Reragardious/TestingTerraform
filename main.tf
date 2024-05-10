ca4c3e20e816ece03141836ea4ade9cd4c94d48d
Reragardious <matthee.alex233@gmail.com>
2024-05-10 18:21:35 +1200

    Update main.tf
    Editing 'create a storage account' to make it usable by changing name and fitting requirements. As well as adding the resource group name in.

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "azurerm_lb" "LoadBalancer" {
 #Create a Storage Account
 resource "azurerm_storage_account" "storageAccount" {
   #name must be lowercase and numbers and between 3 and 24
-  name                     = "GlobalTrust_StorageForVMs_more"
+  name                     = "storageaccountforvm"
   resource_group_name      = "1-0ebe8a23-playground-sandbox"
   location                 = "East US"
   account_tier             = "Standard"
