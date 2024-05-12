76416f135a4cbeb65bf4f938341fea0040756769
Reragardious <matthee.alex233@gmail.com>
2024-05-12 17:21:42 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,13 +6,13 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "globaltrust_banktesting"
   address_space       = ["10.0.0.0/16"]
   location            = "East US"
-  resource_group_name = "1-0ebe8a23-playground-sandbox"
+  resource_group_name = "1-c521cf50-playground-sandbox"
 }
 
 #Create a subnet for frontened
 resource "azurerm_subnet" "front_subnet" {
   name                 = "globaltrust_bank_frontend_subnet"
-  resource_group_name  = "1-0ebe8a23-playground-sandbox"
+  resource_group_name  = "1-c521cf50-playground-sandbox"
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes       = ["10.0.128.0/18"]
 }
@@ -20,7 +20,7 @@ resource "azurerm_subnet" "front_subnet" {
 #Create a subnet for middle
 resource "azurerm_subnet" "middle_subnet" {
   name                 = "globaltrust_bank_middle_subnet"
-  resource_group_name  = "1-0ebe8a23-playground-sandbox"
+  resource_group_name  = "1-c521cf50-playground-sandbox"
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes       = ["10.0.0.0/18"]
 }
@@ -28,7 +28,7 @@ resource "azurerm_subnet" "middle_subnet" {
 #Create a subnet for backend
 resource "azurerm_subnet" "back_subnet" {
   name                 = "globaltrust_bank_backend_subnet"
-  resource_group_name  = "1-0ebe8a23-playground-sandbox"
+  resource_group_name  = "1-c521cf50-playground-sandbox"
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes       = ["10.0.64.0/18"]
 }
@@ -43,7 +43,7 @@ resource "azurerm_public_ip" "publicip" {
   name                = "publicIpForLb"
   #Can use location variable for location below
   location            = "East US"
-  resource_group_name = "1-0ebe8a23-playground-sandbox"
+  resource_group_name = "1-c521cf50-playground-sandbox"
   allocation_method   = "Static"
 }
 
@@ -51,7 +51,7 @@ resource "azurerm_public_ip" "publicip" {
 resource "azurerm_lb" "LoadBalancer" {
     name = "globaltrust_bank_loadBalancer"
     location = "East US"    
-    resource_group_name = "1-0ebe8a23-playground-sandbox"
+    resource_group_name = "1-c521cf50-playground-sandbox"
 
     frontend_ip_configuration {
         name = "PublicIpConnectedto"
@@ -63,7 +63,7 @@ resource "azurerm_lb" "LoadBalancer" {
 resource "azurerm_storage_account" "storageAccount" {
   #name must be lowercase and numbers and between 3 and 24
   name                     = "globalbank582034account"
-  resource_group_name      = "1-0ebe8a23-playground-sandbox"
+  resource_group_name      = "1-c521cf50-playground-sandbox"
   location                 = "East US"
   account_tier             = "Standard"
   account_replication_type = "LRS"
@@ -77,7 +77,7 @@ resource "azurerm_storage_account" "storageAccount" {
 resource "azurerm_network_interface" "networkinterfacemain" {
   name                = "networkinterfaceglobal783"
   location            = "East US"
-  resource_group_name = "1-0ebe8a23-playground-sandbox"
+  resource_group_name = "1-c521cf50-playground-sandbox"
 
   ip_configuration {
     name                          = "testconfiguration"
@@ -91,7 +91,7 @@ resource "azurerm_network_interface" "networkinterfacemain" {
 resource "azurerm_virtual_machine" "main" {
   name                  = "globaltrust_bank_vm"
   location              = "East US"
-  resource_group_name   = "1-0ebe8a23-playground-sandbox"
+  resource_group_name   = "1-c521cf50-playground-sandbox"
   vm_size               = "Standard_DS1_v2"
   network_interface_ids = [azurerm_network_interface.networkinterfacemain.id]
   #virtual machine requires os_disk
@@ -109,5 +109,5 @@ resource "azurerm_virtual_machine" "main" {
 resource "azurerm_availability_set" "availabilitysetforvm" {
   name                = "gloabltrustbankavailset"
   location            = "East US"
-  resource_group_name = "1-0ebe8a23-playground-sandbox"
+  resource_group_name = "1-c521cf50-playground-sandbox"
 }
