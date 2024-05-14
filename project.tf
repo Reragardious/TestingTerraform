0f717fcc29a8140412beb741c337e7d40a45147d
Reragardious <matthee.alex233@gmail.com>
2024-05-14 12:41:03 +1200

    Update project.tf
    Create storage account container within the storage account.

--- project.tf
+++ project.tf
@@ -87,4 +87,10 @@ resource "azurerm_storage_account" "globaltruststorage" {
 # data management, access controls, and compliance with industry regulations, ensuring the 
 # integrity and confidentiality of customer information across global operations.
 
+resource "azurerm_storage_container" "gtb_storage_container" {
+  name                  = "gtb_storage_container"
+  storage_account_name  = azurerm_storage_account.globaltruststorage.name
+  container_access_type = "private"
+}
+
 
