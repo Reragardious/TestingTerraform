5a426bd7939f5ea535fc401c52e5bc9fd5c34d01
Reragardious <matthee.alex233@gmail.com>
2024-05-16 11:26:20 +1200

    Create variables.tf
    Created variables for location and for resource group name. This will be used in the project.tf.

---
+++ variables.tf
@@ -0,0 +1,16 @@
+# to create variables that will be called in project.tf
+
+# for Resource Group's location - This wil be used for all locations
+variable "resource_group_location" {
+  type        = string
+  default     = "East US"
+  description = "Location of the resource group."
+}
+# how to call - var.resource_group_location
+
+# for the Resource Group name, change default to the current resource group name
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "1-bd0b7857-playground-sandbox"
+  description = "The resource group provided by Cloud Guru Sandbox."
+}
