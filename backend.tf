39ccb65d3d9684c6232c4e75d8c6a9aae152c24f
Reragardious <matthee.alex233@gmail.com>
2024-05-16 11:44:45 +1200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 
 resource "azurerm_storage_account" "gtb_backend_storage" {
-  name                     = "backendstorage"
+  name                     = "gtbbackendstorage"
   resource_group_name      = var.resource_group_name
   location                 = var.resource_group_location
   account_tier             = "Standard"
